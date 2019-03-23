a3393dc46cdfac8ce4766b4b6331f3fd8ed09870
Violette <violetter@origo.is>
2019-03-23 21:31:43 +0000

    A: blood test list page

--- lib/pages/body/results_body.dart
+++ lib/pages/body/results_body.dart
@@ -1,6 +1,8 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:selfcare/api/network_util.dart';
 import 'package:selfcare/model/return_data.dart';
+import 'package:selfcare/pages/blood_test_list_page.dart';
 import 'package:selfcare/resources/colors.dart';
 import 'package:selfcare/resources/dimens.dart';
 import 'package:selfcare/resources/images.dart';
@@ -12,7 +14,15 @@ class ResultsBody extends StatelessWidget {
     ReturnData returnData = NetworkUtils.instance.returnData;
     return Column(
       children: <Widget>[
-        _buildResultItem(bloodDropSvg, "Blood tests", true),
+        GestureDetector(
+            onTap: () {
+              Navigator.push(
+                  context,
+                  CupertinoPageRoute(
+                      builder: (context) =>
+                          BloodTestListPage(returnData.bloodworksData)));
+            },
+            child: _buildResultItem(bloodDropSvg, "Blood tests", true)),
         _buildResultItem(radioGraphicSvg, "Radiographics", false),
         _buildResultItem(measurementSvg, "Measurements", false),
         _buildResultItem(avatarSvg, "Self assessment", false),
@@ -20,10 +30,12 @@ class ResultsBody extends StatelessWidget {
     );
   }
 
-
   Widget _buildResultItem(Widget icon, String title, bool newTest) {
     return Padding(
-      padding: const EdgeInsets.only(left: Dimens.mediumSpacing, right: Dimens.mediumSpacing, top: Dimens.mediumSpacing),
+      padding: const EdgeInsets.only(
+          left: Dimens.mediumSpacing,
+          right: Dimens.mediumSpacing,
+          top: Dimens.mediumSpacing),
       child: Card(
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -34,15 +46,16 @@ class ResultsBody extends StatelessWidget {
                   padding: const EdgeInsets.all(Dimens.largeSpacing),
                   child: icon,
                 ),
-                Text(title, style: CustomStyles.treatmentTitleStyle,),
-
+                Text(
+                  title,
+                  style: CustomStyles.treatmentTitleStyle,
+                ),
               ],
             ),
             Visibility(
               visible: newTest,
               child: Container(
-                margin: const EdgeInsets.only(
-                    right: Dimens.mediumSpacing),
+                margin: const EdgeInsets.only(right: Dimens.mediumSpacing),
                 width: Dimens.tinyIconSize,
                 height: Dimens.tinyIconSize,
                 decoration: BoxDecoration(
