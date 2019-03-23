d06cd3727dc83d094296eee84e7a302bd687dfc3
Violette <violetter@origo.is>
2019-03-23 16:08:50 +0000

    A: logout button

--- lib/pages/settings_page.dart
+++ lib/pages/settings_page.dart
@@ -1,4 +1,8 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:selfcare/pages/login_page.dart';
+import 'package:selfcare/resources/colors.dart';
+import 'package:selfcare/resources/dimens.dart';
 
 class SettingsPage extends StatefulWidget {
   @override
@@ -11,6 +15,29 @@ class _SettingsPageState extends State<SettingsPage> {
     return Scaffold(
       appBar: AppBar(
         title: Text('Settings'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(Dimens.mediumSpacing),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            RaisedButton(
+              child: Text('Logout'),
+              textColor: Colors.white,
+              color: CustomColors.accentColor,
+              splashColor: CustomColors.accentColor,
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(Dimens.radius)),
+              padding: const EdgeInsets.all(Dimens.smallSpacing),
+              onPressed: () {
+                Navigator.pushAndRemoveUntil(
+                    context,
+                    CupertinoPageRoute(builder: (context) => LoginPage()),
+                    (Route<dynamic> route) => false);
+              },
+            )
+          ],
+        ),
       ),
     );
   }
