824427f8e394709dde774cab55ce8104f99807b2
ori.annas <anna.stefansdottir@origo.is>
2019-03-23 16:00:01 +0000

    treatment dates

--- lib/pages/my_treatment_page.dart
+++ lib/pages/my_treatment_page.dart
@@ -8,6 +8,7 @@ import 'package:selfcare/api/network_util.dart';
 import 'package:selfcare/model/return_data.dart';
 import 'package:selfcare/resources/dimens.dart';
 import 'package:selfcare/resources/styles.dart';
+import 'package:intl/intl.dart';
 
 class MyTreatmentPage extends StatefulWidget {
   @override
@@ -15,6 +16,7 @@ class MyTreatmentPage extends StatefulWidget {
 }
 
 class _MyTreatmentPageStage extends State<MyTreatmentPage> {
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -39,8 +41,31 @@ class _MyTreatmentPageStage extends State<MyTreatmentPage> {
 
     ReturnData returnData = NetworkUtils.instance.returnData;
     List<TreatmentPlan> treatmentPlans = returnData.treatmentPlans;
+    return Column(
+      children: <Widget>[
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(Dimens.smallSpacing),
+              child: Icon(Icons.arrow_back_ios, size: Dimens.tinyIconSize,),
+            ),
+            Text(getDate(), style: CustomStyles.dateTitle,),
+            Padding(
+              padding: const EdgeInsets.all(Dimens.smallSpacing),
+              child: Icon(Icons.arrow_forward_ios, size: Dimens.tinyIconSize,),
+            ),
+          ],
+        ),
+        _buildTreatmentPlans(treatmentPlans),
+      ],
+    );
+  }
+
+  Widget _buildTreatmentPlans(List<TreatmentPlan> treatmentPlans) {
     return ListView.builder
       (
+        shrinkWrap: true,
         itemCount: treatmentPlans.length,
         itemBuilder: (BuildContext context, int index) {
           return Column(
@@ -50,7 +75,7 @@ class _MyTreatmentPageStage extends State<MyTreatmentPage> {
                 padding: const EdgeInsets.all(Dimens.smallSpacing),
                 child: new Text(treatmentPlans[index].type,  style: CustomStyles.treatmentTitleStyle,),
               ),
-              _buildTreatmentPlan(treatmentPlans[index].treatmentItems),
+              _buildTreatmentItems(treatmentPlans[index].treatmentItems),
               Divider(color: Colors.grey,)
             ],
           );
@@ -58,9 +83,10 @@ class _MyTreatmentPageStage extends State<MyTreatmentPage> {
     );
   }
   
-  Widget _buildTreatmentPlan(List<TreatmentItem> treatmentItems) {
+  Widget _buildTreatmentItems(List<TreatmentItem> treatmentItems) {
     return ListView.builder(
         shrinkWrap: true,
+        physics: const NeverScrollableScrollPhysics(),
         itemCount: treatmentItems.length,
         itemBuilder: (BuildContext context, int index) {
           return Column(
@@ -80,6 +106,7 @@ class _MyTreatmentPageStage extends State<MyTreatmentPage> {
   Widget _buildTreatments(List<Treatment> treatments) {
     return ListView.builder(
         shrinkWrap: true,
+        physics: const NeverScrollableScrollPhysics(),
         itemCount: treatments.length,
         itemBuilder: (BuildContext context, int index) {
           return Row(
@@ -96,5 +123,10 @@ class _MyTreatmentPageStage extends State<MyTreatmentPage> {
           );
         }
     );
+  }
+
+  String getDate() {
+    DateTime now = DateTime.now();
+    return DateFormat('EEEE dd.MM.yyyy').format(now);
   }
 }
